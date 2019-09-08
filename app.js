3924ac2662510dbb4f5cd786476cc366ad24ed6f
PacocaApps <gpaaadm@gmail.com>
2019-09-08 17:58:50 -0300

    test

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-var http = require('http');
-var fs = require('fs');
+var http = require('http');  
+var url = require('url');  
+var fs = require('fs');  
+var server = http.createServer(function(request, response) {  
+    var path = url.parse(request.url).pathname;  
+    switch (path) {  
+        case '/':  
+         
 
-function onRequest(request, response){
-    response.writeHead(200, {'Content-Type':'text/html'});
-    fs.readFile('./index.html',null,function(error,data){
+fs.readFile('./index.html',null,function(error,data){
     if(error) {
         response.writeHead(404);
         response.write("File Not Found Error Type 404 ")
@@ -12,10 +16,57 @@ function onRequest(request, response){
         response.write(data);
         
     }
-    response.end();
+            break;  
+        case '/login.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break;  
+        case '/penis.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break;  
+        default:  
+            response.writeHead(404);  
+            response.write("opps this doesn't exist - 404");  
+            response.end();  
+            break;  
+    }  
+});  
+server.listen(8082); 
 
 
-    })
 
-}
-http.createServer(onRequest).listen(21094);
+
+
+
+
+
+
+
+
+
+
+
+
