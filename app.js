09864cd27b2476d2ff28606daa08df609058f849
PacocaApps <gpaaadm@gmail.com>
2019-09-09 17:50:50 -0300

    requesthandler1Removal

--- app.js
+++ app.js
@@ -15,20 +15,6 @@ var server = http.createServer(function(request, response) {
 
 
 
-if (request.method === "POST") {
-
-    var body = "";
-    request.on("data", function (chunk) {
-        body += chunk;
-    });
-
-    request.on("end", function(){
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.end(body);
-    });
-}
-
-
 
 
 
